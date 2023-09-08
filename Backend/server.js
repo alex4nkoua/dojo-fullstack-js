9ad688ffa5c948193568589c2ce274f2192c986a
Alexandre Nkoua <alexborn2code@gmail.com>
2023-09-08 11:27:23 +0200

    ajout: stabilité server http

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,61 @@
-const http = require('http');
+const http  = require('http');
+const app   = require('./app');
 
-const server = http.createServer((req, res) => {
-    res.end(`Voilà la réponse du server`);
+
+
+// normalizePort renvoie un port valide, qu'il soit fourni sous la forme d'un numéro ou d'une chaîne ;
+const normalizePort = val => {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+      return val;
+    }
+    if (port >= 0) {
+      return port;
+    }
+    return false;
+  };
+
+
+const port = normalizePort(process.env.PORT ||'3000');
+
+// on set le port de lapplication express
+app.set('port', port);
+
+// errorHandler recherche les différentes erreurs et les gère de manière appropriée.
+// Elle est ensuite enregistrée dans le serveur ;
+const errorHandler = error => {
+if (error.syscall !== 'listen') {
+    throw error;
+}
+const address = server.address();
+const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+switch (error.code) {
+    case 'EACCES':
+    console.error(bind + ' requires elevated privileges.');
+    process.exit(1);
+    break;
+    case 'EADDRINUSE':
+    console.error(bind + ' is already in use.');
+    process.exit(1);
+    break;
+    default:
+    throw error;
+}
+};
+
+
+
+// création du server http qui servira l'app express
+const server = http.createServer(app);
+
+// gestion des erreurs
+server.on('error', errorHandler);
+
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
 });
 
-server.listen(process.env.PORT || 5000, () => {
-    console.log(`Le server est ouvert sur le port: ${process.env.PORT || 5000}`);
-});
+server.listen(port);
